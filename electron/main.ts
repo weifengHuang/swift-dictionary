c1f76272ff7a9616446e1dba79783ee6bf41244b
breeze <463815567@qq.com>
2023-08-26 16:33:30 +0800

    init

---
+++ electron/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import isDev from 'electron-is-dev';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      // enableRemoteModule: true,
+    },
+  });
+
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
