c8ff7453a1fc48847e0fa24d9c8c859b873fb085
breeze <463815567@qq.com>
2024-05-05 17:25:48 +0800

    fix: support message when not load dictionary

--- src/renderer/pages/dictionary/index.tsx
+++ src/renderer/pages/dictionary/index.tsx
@@ -5,22 +5,28 @@ import { useAtom } from 'jotai';
 import { searchResultsAtom } from './store';
 import { DisplayContent } from './displayContent';
 import {Toolbar} from './toolbar';
+import { message } from 'antd';
+import debounce from 'lodash.debounce'
 
 export const DictionaryPage: React.FC = () => {
   const [_, setSearchResults] = useAtom(searchResultsAtom);
   const handleSearch = async (value: string) => {
     // 这里是模拟数据，实际你需要使用真实的搜索服务
-    const results = await window.ipcRenderer.invoke<unknown[]>(
-      'search-words',
-      value
-    );
-    setSearchResults(results.map((item: unknown) => item.toString()));
+    try {
+      const results = await window.ipcRenderer.invoke<unknown[]>(
+        'search-words',
+        value
+      );
+      setSearchResults(results.map((item: unknown) => item.toString()));
+    } catch (error) {
+       message.error('查询字典失败，检查是否有导入词典')
+    }
   };
   return (
     <div className="flex">
       <div className="w-1/4 p-4">
         <SearchBar 
-          onSearch={handleSearch} 
+          onSearch={debounce(handleSearch, 500)} 
         />
         <SearchResults />
       </div>
